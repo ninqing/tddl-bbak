ecfcc9e80f11be44b6ea73c0e85a87b62314bea6
ningqing <ningqing1983@gmail.com>
2016-07-30 01:54:37 +0800

    group层还是存在错误,明天再定位问题.

--- tddl-sample/src/main/java/com/taobao/tddl/sample/GroupSample.java
+++ tddl-sample/src/main/java/com/taobao/tddl/sample/GroupSample.java
@@ -1,10 +1,17 @@
 package com.taobao.tddl.sample;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import com.taobao.tddl.group.jdbc.TGroupDataSource;
 
 public class GroupSample {
-	public static void main(String[] args) {
-		TGroupDataSource tg = new TGroupDataSource("test_group_key", "test_app");
+	public static void main(String[] args) throws Throwable {
+		TGroupDataSource tg = new TGroupDataSource();
+		tg.setAppName("test_app");
+		tg.setDbGroupKey("test_group_key");
 		tg.init();
+		System.out.println(
+				tg.getConnection().prepareStatement("insert into t1 values(1,'name1')").execute());
+		tg.destroyDataSource();
 	}
 }
--- tddl-sample/src/main/java/com/taobao/tddl/sample/UnitConfigDataHandlerTest.java
+++ tddl-sample/src/main/java/com/taobao/tddl/sample/UnitConfigDataHandlerTest.java
@@ -18,11 +18,11 @@ public class UnitConfigDataHandlerTest extends UnitConfigDataHandler {
 		meta.put("com.taobao.tddl.atom.global.dbk2",
 				"ip=127.0.0.1\nport=3307\ndbName=test1\ndbType=mysql\ndbStatus=RW");
 		meta.put("com.taobao.tddl.atom.app.test_app.dbk2",
-				"userName=tddl\nminPoolSize=1\nmaxPoolSize=2\nidleTimeout=1000\nblockingTimeout=1000\nconnectionProperties=rewriteBatchedStatements=true&characterEncoding=UTF8&connectTimeout=1000&autoReconnect=true&socketTimeout=12000");
+				"userName=tddl\nminPoolSize=10\nmaxPoolSize=20\nidleTimeout=10\nblockingTimeout=5\nconnectionProperties=rewriteBatchedStatements=true&characterEncoding=UTF8&connectTimeout=1000&autoReconnect=true&socketTimeout=12000");
 		meta.put("com.taobao.tddl.atom.app.test_app.dbk1",
-				"userName=tddl\nminPoolSize=1\nmaxPoolSize=2\nidleTimeout=1000\nblockingTimeout=1000\nconnectionProperties=rewriteBatchedStatements=true&characterEncoding=UTF8&connectTimeout=1000&autoReconnect=true&socketTimeout=12000");
+				"userName=tddl\nminPoolSize=10\nmaxPoolSize=20\nidleTimeout=10\nblockingTimeout=5\nconnectionProperties=rewriteBatchedStatements=true&characterEncoding=UTF8&connectTimeout=1000&autoReconnect=true&socketTimeout=12000");
 		meta.put("com.taobao.tddl.atom.passwd.test1.mysql.tddl",
-				"encPasswd=test");
+				"encPasswd=tddl");
 	}
 
 	@Override
