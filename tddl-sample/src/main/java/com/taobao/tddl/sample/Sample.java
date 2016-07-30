e73e9cd75cbd254ca18793d62cf7cf44dd744617
ningqing <ningqing1983@gmail.com>
2016-07-31 00:42:52 +0800

    还没调通.

--- tddl-sample/src/main/java/com/taobao/tddl/sample/Sample.java
+++ tddl-sample/src/main/java/com/taobao/tddl/sample/Sample.java
@@ -1,11 +1,8 @@
 package com.taobao.tddl.sample;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
+
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.taobao.tddl.common.exception.TddlException;
 import com.taobao.tddl.matrix.jdbc.TDataSource;
@@ -18,38 +15,17 @@ public class Sample {
 
         ds.setAppName("test_app");
         ds.setRuleFile("rule.xml");
-
-        Map cp = new HashMap();
-        cp.put("ALLOW_TEMPORARY_TABLE", "True");
-
-        ds.setConnectionProperties(cp);
-
         ds.init();
 
         System.out.println("init done");
 
-        Connection conn = ds.getConnection();
-        PreparedStatement ps = conn.prepareStatement("SELECT a.custId, a.id,b.adgroupid, SUM(c.cost) / SUM(c.click) AS ppc,c.thedate, c.memberid, c.campaignid, c.productlineid, c.adgroupid, SUM(c.impression), SUM(c.cost), SUM(c.click), SUM(c.click) / SUM(c.impression) AS ctr, b.title, a.onlinestate, a.reason FROM Lunaadgroup a join lunaadgroupinfo b on a.id=b.adgroupid  LEFT JOIN rpt_solar_adgroup_ob c ON a.id = c.adgroupid where a.custid='1102000884' and b.custid='1102000884' and b.title like '%办公室%'  GROUP BY c.thedate, c.memberid, c.campaignid, c.productlineid, c.adgroupid HAVING ppc > 1 ORDER BY ppc DESC");
-
-        long start = System.currentTimeMillis();
-        ResultSet rs = ps.executeQuery();
-        while (rs.next()) {
-            StringBuilder sb = new StringBuilder();
-            int count = rs.getMetaData().getColumnCount();
-            for (int i = 1; i <= count; i++) {
-
-                String key = rs.getMetaData().getColumnLabel(i);
-                Object val = rs.getObject(i);
-                sb.append("[" + rs.getMetaData().getTableName(i) + "." + key + "->" + val + "]");
-            }
-            System.out.println(sb.toString());
-        }
-
-        rs.close();
-        ps.close();
-        conn.close();
-
-        System.out.println("done " + (System.currentTimeMillis() - start));
+        JdbcTemplate jt0 = new JdbcTemplate(ds);
+		jt0.update("delete from test_table");
+		System.out.println(jt0.update("insert into test_table values(1,'name1')"));
+		System.out.println(jt0.update("insert into test_table values(2,'name1')"));
+		System.out.println(jt0.update("insert into test_table values(3,'name1')"));
+		System.out.println(jt0.update("insert into test_table values(4,'name1')"));
+		
     }
 
 }
